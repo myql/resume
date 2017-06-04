3e59c02a5813ccd0c3395d478fa1f53388ec348e
myql <276466206@qq.com>
2017-06-04 22:06:50 +0800

    some

--- yemian/js/main.js
+++ yemian/js/main.js
@@ -1,11 +1,6 @@
 var $navbar=$('.navbar'),$navbarNav=$('.navbar-nav');
 var $container=$('#container');
-var $navbarBrand=$('.navbar-brand');
-$navbarNav.children().on('click',function(){
-		var idx=$(this).index();
-		$navbarNav.children().removeClass('active').eq(idx).
-		addClass('active');
-	})
+
 var windowTop=$(window).scrollTop();
 if((windowTop)>10){
 		$navbar.css({
@@ -16,17 +11,12 @@ if((windowTop)>10){
 		background: 'none'
 		})	
 	}
-	
-
 	for(var i=0;i<$container.children().length-1;i++){
 	var divTop=$container.children().eq(i).offset().top;
 	var divTop1=$container.children().eq(0).offset().top;
-	if(windowTop>divTop){
-		$navbarNav.children().removeClass('active').eq(i).addClass('active');
-		
-	}else if(windowTop<divTop1){
-		$navbarNav.children().removeClass('active')
-	}
+	if(windowTop>divTop) {
+        $navbarNav.children().removeClass('active').eq(i).addClass('active');
+    }
 }
 $(window).scroll(function(){
 	var windowTop=$(window).scrollTop();
@@ -43,13 +33,20 @@ $(window).scroll(function(){
 	for(var i=0;i<($container.children().length-1);i++){
 	var divTop=$container.children().eq(i).offset().top;
 	var divTop1=$container.children().eq(0).offset().top;
-	if((windowTop+$(window).height())>divTop){
+        console.log(windowTop-divTop)
+	if((windowTop+160)>=divTop){
 		$navbarNav.children().removeClass('active').eq(i).addClass('active');
-		
-	}else if((windowTop+$(window).height())<divTop1) {
-		$navbarNav.children().removeClass('active')
+
 	}
-	
+	else if((windowTop)<divTop1) {
+        $navbarNav.children().removeClass('active')
+    }
 }
+})
+$navbarNav.children().on('click',function(){
+    var idx=$(this).index();
+    $navbarNav.children().removeClass('active').eq(idx).
+    addClass('active');
+    console.log($navbarNav.children().eq(idx).siblings().attr('class'))
 })
 
